c9d673dd5d8976c4d5830b3f59be42efc9c9962b
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2024-03-11 21:35:12 +0200

    The rule of the Big 5

--- lectures/common_example/person.cpp
+++ lectures/common_example/person.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 #include <cstring>
 
-// Вътрешен метод за копиране. Вика се само от определени места и не може да доведе до проблем, защото това се гарантира от разработчика.
-// За името НЕ трябва да има вече заделена памет.
 void Person::copy(char const* name) {
 	firstName = new char[strlen(name) + 1];
 	strcpy(firstName, name);
@@ -17,4 +15,44 @@ void Person::print() const {
 Person::Person(char const * name, unsigned const& ageData) {
 	age = ageData;
 	copy(name);
+}
+
+Person::~Person() {
+	delete[] firstName;
+}
+
+Person::Person(Person const& other) { 
+	age = other.age;
+	copy(other.firstName);
+}
+
+Person::Person(Person && other) {
+	age = other.age;
+	firstName = other.firstName;
+	
+	other.firstName = nullptr;
+	other.age = 0;
+}
+
+Person& Person::operator=(Person const& other) {
+	if (this != &other) {
+		delete[] firstName;
+		copy(other.firstName);
+	}
+
+	return *this;
+}
+
+Person& Person::operator=(Person && other) {
+	if (this != &other) {
+		age = other.age;
+
+		delete[] firstName;
+		firstName = other.firstName;
+
+		other.firstName = nullptr;
+		other.age = 0;
+	}
+
+	return *this;
 }
