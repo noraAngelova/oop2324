d1e0e8e97d6a59a087c461bbb6bfdd769b6afd26
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2024-04-16 10:04:54 +0300

    Create student and teacher objects

--- lectures/common_example/main.cpp
+++ lectures/common_example/main.cpp
@@ -1,13 +1,17 @@
 #include "person.hpp"
+#include "student.hpp"
+#include "teacher.hpp"
 
 int main() {
-    // Създава обект с конструктоп по подразбиране
+    // Създава обект с конструктор по подразбиране
     Person p; 
+    std::cout << "Default Person object: ";
     p.print();
     std::cout << std::endl;
 
     // Създава обект с конструктор за копиране
     Person p2 = p;
+    std::cout << "Person object (copy constructor): ";
     p2.print();
     std::cout << std::endl;
 
@@ -16,8 +20,9 @@ int main() {
 
     // Извършва присвояване между два създадени обекта чрез оператора за присвояване
     p2 = p3;
+    std::cout << "Person object (operator=): ";
     p2.print();
-
+    std::cout << std::endl;
 
     // Сравнява два обекта
     if (p == p2) {
@@ -26,14 +31,52 @@ int main() {
 
     // Сравнява два обекта по възраст
     if (p < p2) {
-        std::cout << "\n p is younger than p2";
+        std::cout << "p is younger than p2" << std::endl;
     }
     
     // Въвежда и извежда информация за потребител от поток
-    Person streamPerson;
+    /*Person streamPerson;
     std::cout << "\n Enter a user data - capacity, name and age:" << std::endl;
     std::cin >> streamPerson;
-    std::cout << streamPerson << std::endl;
+    std::cout << streamPerson << std::endl;*/
+
+    // Създава обект с конструктор с параметри
+    Student st("Georgi Ivanov", 19, "0MI00013422");
+    std::cout << "Student object: ";
+    st.print();
+
+    std::cout << std::endl;
+
+    // Създава обект с конструктор за копиране
+    std::cout << "Student object (copy constructor): ";
+    Student st2 = st;
+    st2.print();
+
+    std::cout << std::endl;
+
+    // Извършва присвояване между два създадени обекта чрез оператора за присвояване
+    Student st3;
+    st3 = st;
+    std::cout << "Student object (operator=): ";
+    st3.print();
+
+    std::cout << std::endl;
+
+    // Създава обект с конструктор без параметри
+    Teacher t1;
+    t1.print();
+    std::cout << std::endl;
+
+    // Създава обект с конструктор за копиране
+    Teacher t2 = t1;
+    t2.print();
+    std::cout << std::endl;
+
+    // Извършва присвояване между два създадени обекта чрез оператора за присвояване
+    Teacher t3;
+    t3 = t1;
+    t3.print();
+    std::cout << std::endl;
 
     return 0;
 }
